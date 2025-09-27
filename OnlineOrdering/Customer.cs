5dc268cf8fe754c89a298ba3f56f320160f40a84
ivang934 <163324461+ivang934@users.noreply.github.com>
2025-09-26 23:43:28 -0300

    Create Customer.cs

---
+++ OnlineOrdering/Customer.cs
@@ -0,0 +1,28 @@
+
+public class Customer
+{
+    private string _name;
+    private Address _address; // Contains an Address object.
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    
+    public bool LivesInUSA()
+    {
+        return _address.IsInUSA();
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetAddressString()
+    {
+        return _address.GetFullAddress();
+    }
+}
